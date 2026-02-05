6803adb7d295ef04fb477ceffd3b3798913731d9
bhrgvbhrgv <bhargavdas21th@gmail.com>
2026-02-05 23:35:52 +0530

    CORS

--- server/server.js
+++ server/server.js
@@ -14,7 +14,12 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins (for now) to fix deployment issues
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
